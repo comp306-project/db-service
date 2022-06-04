e7ca2e2b2f2d810bcedabecd8bd49d9942170868
Sema-gure <79368059+Sema-gure@users.noreply.github.com>
2022-06-05 00:11:42 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -141,6 +141,14 @@ WHERE RACES.race_id = RESULTS.race_id AND RESULTS.race_id = RACES.race_id AND DR
 GROUP BY DRIVERS.driver_id
 ORDER BY AVG(RESULTS.position_order) ASC ;
 
+SELECT DISTINCT(DRIVERS.forename), DRIVERS.surname, RACES.year, DRIVERS.nationality, COnstructors.nationality
+FROM DRIVERS, Constructors, RESULTS,RACES
+WHERE DRIVERS.nationality = Constructors.nationality AND RESULTS.race_id = RACES.race_id AND 
+RESULTS.constructor_id = Constructors.constructor_id AND DRIVERS.driver_id = RESULTS.driver_id AND DRIVERS.driver_id IN 
+(SELECT DISTINCT(DRIVERS.driver_id)
+FROM DRIVERS, RESULTS, RACES
+WHERE RESULTS.position_order = 1 AND RESULTS.race_id = RACES.race_id AND RESULTS.driver_id = DRIVERS.driver_id)
+
 
 # ======== ATA============
 ### Q1
